8cb365d6a12df209e9dbef2ef4fad8d5da9e3d99
Taylor Southwick <twsouthwick@outlook.com>
2017-08-30 11:48:47 -0700

    Add simple Analyzer helper

---
+++ src/ApiCompat/Analyzer.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.Cci;
+using Microsoft.Cci.Comparers;
+using Microsoft.Cci.Differs;
+using Microsoft.Cci.Extensions;
+using Microsoft.Cci.Filters;
+using Microsoft.Cci.Mappings;
+using Microsoft.Cci.Writers;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Composition.Hosting;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace ApiCompat
+{
+    public class ExportCciSettings
+    {
+        public static IEqualityComparer<ITypeReference> StaticSettings { get; set; }
+        public ExportCciSettings()
+        {
+            Settings = StaticSettings;
+        }
+
+        [Export(typeof(IEqualityComparer<ITypeReference>))]
+        public IEqualityComparer<ITypeReference> Settings { get; set; }
+    }
+
+    public class Analyzer
+    {
+        private readonly TextWriter _log;
+
+        public Analyzer(TextWriter log)
+        {
+            _log = log;
+
+            Trace.Listeners.Add(new TextWriterTraceListener(_log)
+            {
+                Filter = new EventTypeFilter(SourceLevels.Error | SourceLevels.Warning)
+            });
+        }
+
+        public void Analyze(IEnumerable<string> version1Assemblies, IEnumerable<string> version2Assemblies)
+        {
+            BaselineDifferenceFilter filter = GetBaselineDifferenceFilter();
+            NameTable sharedNameTable = new NameTable();
+            HostEnvironment contractHost = new HostEnvironment(sharedNameTable);
+            contractHost.UnableToResolve += new EventHandler<UnresolvedReference<IUnit, AssemblyIdentity>>(contractHost_UnableToResolve);
+            contractHost.ResolveAgainstRunningFramework = true;
+            contractHost.UnifyToLibPath = true;
+            contractHost.AddLibPaths(HostEnvironment.SplitPaths(s_contractLibDirs));
+            IEnumerable<IAssembly> contractAssemblies = contractHost.LoadAssemblies(string.Join(";", version1Assemblies), s_contractCoreAssembly);
+
+            if (s_ignoreDesignTimeFacades)
+                contractAssemblies = contractAssemblies.Where(a => !a.IsFacade());
+
+            HostEnvironment implHost = new HostEnvironment(sharedNameTable);
+            implHost.UnableToResolve += new EventHandler<UnresolvedReference<IUnit, AssemblyIdentity>>(implHost_UnableToResolve);
+            implHost.ResolveAgainstRunningFramework = s_resolveFx;
+            implHost.UnifyToLibPath = s_unifyToLibPaths;
+            implHost.AddLibPaths(version2Assemblies);
+            if (s_warnOnMissingAssemblies)
+                implHost.LoadErrorTreatment = ErrorTreatment.TreatAsWarning;
+
+            // The list of contractAssemblies already has the core assembly as the first one (if _contractCoreAssembly was specified).
+            IEnumerable<IAssembly> implAssemblies = implHost.LoadAssemblies(contractAssemblies.Select(a => a.AssemblyIdentity), s_warnOnIncorrectVersion);
+
+            // Exit after loading if the code is set to non-zero
+            if (DifferenceWriter.ExitCode != 0)
+                return;
+
+            ICciDifferenceWriter writer = GetDifferenceWriter(_log, filter);
+            writer.Write(s_implDirs, implAssemblies, s_contractSet, contractAssemblies);
+        }
+
+        public IEnumerable<string> GetRules()
+        {
+            var c = GetCompositionHost();
+            var rules = c.GetExports<IDifferenceRule>();
+
+            return rules.Select(r => r.GetType().Name).ToList();
+        }
+
+        private static CompositionHost GetCompositionHost()
+        {
+            var configuration = new ContainerConfiguration().WithAssembly(typeof(Analyzer).GetTypeInfo().Assembly);
+            return configuration.CreateContainer();
+        }
+
+        private static BaselineDifferenceFilter GetBaselineDifferenceFilter()
+        {
+            // TODO: allow baseline
+            return null;
+        }
+
+        private static void implHost_UnableToResolve(object sender, UnresolvedReference<IUnit, AssemblyIdentity> e)
+        {
+            Trace.TraceError("Unable to resolve assembly '{0}' referenced by the implementation assembly '{1}'.", e.Unresolved, e.Referrer);
+        }
+
+        private static void contractHost_UnableToResolve(object sender, UnresolvedReference<IUnit, AssemblyIdentity> e)
+        {
+            Trace.TraceError("Unable to resolve assembly '{0}' referenced by the contract assembly '{1}'.", e.Unresolved, e.Referrer);
+        }
+
+        private static ICciComparers GetComparers()
+        {
+            if (!string.IsNullOrEmpty(s_remapFile))
+            {
+                if (!File.Exists(s_remapFile))
+                {
+                    throw new FileNotFoundException("ERROR: RemapFile {0} was not found!", s_remapFile);
+                }
+                return new NamespaceRemappingComparers(s_remapFile);
+            }
+            return CciComparers.Default;
+        }
+
+        private static ICciFilter GetCciFilter(bool enforcingMdilRules, bool excludeNonBrowsable)
+        {
+            if (enforcingMdilRules)
+            {
+                return new MdilPublicOnlyCciFilter()
+                {
+                    IncludeForwardedTypes = true
+                };
+            }
+            else if (excludeNonBrowsable)
+            {
+                return new PublicEditorBrowsableOnlyCciFilter()
+                {
+                    IncludeForwardedTypes = true
+                };
+            }
+            else
+            {
+                return new PublicOnlyCciFilter()
+                {
+                    IncludeForwardedTypes = true
+                };
+            }
+        }
+
+        private static IMappingDifferenceFilter GetDiffFilter(ICciFilter filter)
+        {
+            return new MappingDifferenceFilter(GetIncludeFilter(), filter);
+        }
+
+        private static Func<DifferenceType, bool> GetIncludeFilter()
+        {
+            return d => d != DifferenceType.Unchanged;
+        }
+
+        private static ICciDifferenceWriter GetDifferenceWriter(TextWriter writer, IDifferenceFilter filter)
+        {
+            CompositionHost container = GetCompositionHost();
+
+            Func<IDifferenceRuleMetadata, bool> ruleFilter =
+                delegate (IDifferenceRuleMetadata ruleMetadata)
+                {
+                    if (ruleMetadata.MdilServicingRule && !s_mdil)
+                        return false;
+                    return true;
+                };
+
+            if (s_mdil && s_excludeNonBrowsable)
+            {
+                Trace.TraceWarning("Enforcing MDIL servicing rules and exclusion of non-browsable types are both enabled, but they are not compatible so non-browsable types will not be excluded.");
+            }
+
+            MappingSettings settings = new MappingSettings();
+            settings.Comparers = GetComparers();
+            settings.Filter = GetCciFilter(s_mdil, s_excludeNonBrowsable);
+            settings.DiffFilter = GetDiffFilter(settings.Filter);
+            settings.DiffFactory = new ElementDifferenceFactory(container, ruleFilter);
+            settings.GroupByAssembly = s_groupByAssembly;
+            settings.IncludeForwardedTypes = true;
+
+            if (filter == null)
+            {
+                filter = new DifferenceFilter<IncompatibleDifference>();
+            }
+
+            ICciDifferenceWriter diffWriter = new DifferenceWriter(writer, settings, filter);
+            ExportCciSettings.StaticSettings = settings.TypeComparer;
+
+            // Always compose the diff writer to allow it to import or provide exports
+            container.SatisfyImports(diffWriter);
+
+            return diffWriter;
+        }
+
+        private static string s_contractCoreAssembly;
+        private static string s_contractSet;
+        private static string s_implDirs;
+        private static string s_contractLibDirs;
+        private static bool s_listRules;
+        private static string s_outFile;
+        private static string s_baselineFileName;
+        private static string s_remapFile;
+        private static bool s_groupByAssembly = true;
+        private static bool s_mdil;
+        private static bool s_resolveFx;
+        private static bool s_unifyToLibPaths = true;
+        private static bool s_warnOnIncorrectVersion;
+        private static bool s_ignoreDesignTimeFacades;
+        private static bool s_excludeNonBrowsable;
+        private static bool s_warnOnMissingAssemblies;
+    }
+}
--- src/NuGetCompatAnalyzer/Program.cs
+++ src/NuGetCompatAnalyzer/Program.cs
@@ -6,7 +6,16 @@ namespace NuGetCompatAnalyzer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var analyzer = new ApiCompat.Analyzer(Console.Out);
+
+            foreach (var name in analyzer.GetRules())
+            {
+                Console.WriteLine(name);
+            }
+
+            var _604 = @"C:\Users\tasou\.nuget\packages\newtonsoft.json\6.0.4\lib\net45\Newtonsoft.Json.dll";
+            var _10 = @"C:\Users\tasou\.nuget\packages\newtonsoft.json\10.0.3\lib\net45\Newtonsoft.Json.dll";
+            analyzer.Analyze(new[] { _604 }, new[] { _10 });
         }
     }
 }
